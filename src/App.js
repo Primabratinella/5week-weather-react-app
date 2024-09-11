4b9f36b88d94eda8bf41cd6c563f032a6eea284c
Primabratinella <karen.pearl@hotmail.com>
2024-09-11 15:49:56 -0400

    Added GitHub repo url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App
+      </h1>
+      <footer>
+      This project is coded by <a href='https://www.shecodes.io/graduates/17245-karen-berglund' target='_blank'>
+      Karen Berglund</a> {" "}
+      and is open-sourced on {" "} 
+      <a 
+      href='https://github.com/Primabratinella/5week-weather-react-app'
+      target='_blank'
+      >
+      GitHub
+      </a>
+      </footer>
     </div>
   );
 }
