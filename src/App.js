fc91f80c682b81c0ee6c798922b0dee272634881
Primabratinella <karen.pearl@hotmail.com>
2024-09-12 22:29:35 -0400

    added weather.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-
-
-      <h1>Weather App
-      </h1>
+      <Weather />
       <footer>
-      This project is coded by <a href='https://www.shecodes.io/graduates/17245-karen-berglund' target='_blank'>
+      This project is coded by <a 
+      href='https://www.shecodes.io/graduates/17245-karen-berglund' 
+      target='_blank' rel="noreferrer">
       Karen Berglund</a> {" "}
       and is open-sourced on {" "} 
       <a 
       href='https://github.com/Primabratinella/5week-weather-react-app'
-      target='_blank'
-      >
+      target='_blank' rel="noreferrer">
       GitHub
       </a>
       </footer>
