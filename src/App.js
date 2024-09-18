9cc06ed7713c2af6cf949592b5e2d07461c45192
Primabratinella <karen.pearl@hotmail.com>
2024-09-18 10:39:17 -0400

    Added links in App.js

--- src/App.js
+++ src/App.js
@@ -5,22 +5,25 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
-      <Weather />
-      <footer>
-      This project is coded by <a 
-      href='https://www.shecodes.io/graduates/17245-karen-berglund' 
-      target='_blank' rel="noreferrer">
-      Karen Berglund</a> {" "}
-      and is open-sourced on {" "} 
-      <a 
-      href='https://github.com/Primabratinella/5week-weather-react-app'
-      target='_blank' rel="noreferrer">
-      GitHub
-      </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is coded by 
+          <a 
+            href="https://www.shecodes.io/graduates/17245-karen-berglund" 
+            target="_blank" 
+            rel="noreferrer">
+            Karen Berglund
+          </a> {" "}
+          and is open-sourced on {" "} 
+          <a 
+            href="https://github.com/Primabratinella/5week-weather-react-app"
+            target="_blank" 
+            rel="noreferrer">
+            GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
 }
-
--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,46 @@
 import React from "react";
+import axios from "axios";
+import "./Weather.css";
 
-export default function Weather (){
-    return
-     <div>Hello React World!</div>;
-    }
+export default function Weather() {
+   
+  return (  
+  <div className="Weather">
+    <form>
+        <div className="row">
+            <div className="col-9">
+        <input 
+        type="search" 
+        placeholder="Search city..."
+        className="form-control"
+        />
+        </div>
+        <div className="col-3">
+        <input type="submit" value="Search" className="btn btn-primary" />
+        </div>
+        </div>
+    </form>
+    <h1>Maryland</h1>
+    <ul>
+        <li>Friday 12:55</li>
+        <li>Mostly Cloudy</li>
+    </ul>
+    <div className="row">
+        <div className="col-6">
+          <img
+          src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+          alt="Monday"
+          /> 
+            70°
+        </div>
+        <div className="col-6">
+            <ul>
+                <li>Precipitation: 15%</li>
+                <li>Humidity: 80%</li>
+                <li>Wind: 30 mph</li>
+            </ul>
+        </div>
+    </div>
+    </div>
+  );
+}
